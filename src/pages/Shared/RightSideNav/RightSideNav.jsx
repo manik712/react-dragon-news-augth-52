5fb3430928f356b2b62501ed508890bfc623b1a6
manik712 <ranineela71@gmail.com>
2023-10-03 21:54:07 -0700

    ok

--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -4,8 +4,8 @@ import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 const RightSideNav = () => {
   return (
     <div>
-      <div className="p-4 space-y-3 mb-6">
-        <h2 className="text-3xl">Login With</h2>
+      <div className="p-4  mb-6">
+        <h2 className="text-3xl mb-4">Login With</h2>
 
         <button className="btn btn-outline w-full">
           <BsGoogle></BsGoogle>Login with Google
@@ -14,15 +14,15 @@ const RightSideNav = () => {
           <FaGithub></FaGithub>Login with Github
         </button>
       </div>
-      <div className="p-4 space-y-3 mb-6">
+      <div className="p-4  mb-6">
         <h2 className="text-3xl">Find us on</h2>
-        <a className="flex items-center gap-2 p-4" href="">
+        <a className="flex items-center gap-2 p-4 border rounded-t-lg" href="">
           <FaFacebook className=""></FaFacebook>Facebook
         </a>
-        <a className="flex items-center gap-2 p-4" href="">
+        <a className="flex items-center gap-2 p-4 border rounded-t-lg" href="">
           <FaTwitter className="text-blue-500"></FaTwitter>Twitter
         </a>
-        <a className="flex items-center gap-2 p-4 " href="">
+        <a className="flex items-center gap-2 p-4 border rounded-t-lg " href="">
           <FaInstagram className="text-fuchsia-600"></FaInstagram>Instagram
         </a>
       </div>
