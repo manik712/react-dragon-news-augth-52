bdf0346c4cfa1b86bf8f302dce44a1af51a632d9
manik712 <ranineela71@gmail.com>
2023-10-05 10:30:12 -0700

    added

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,10 +1,12 @@
+import { useParams } from "react-router-dom";
 import Header from "../Shared/Header/Header";
 import Navbar from "../Shared/Navbar/Navbar";
 import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 
 const News = () => {
-  
+    const {id} = useParams();
+    console.log(id);
   return (
     <div>
       <Header></Header>
